e527ecf50c98e2ac181d6d197d0f798a7ed87897
Santiago Seifert <aquilescanta@google.com>
2020-11-30 21:26:02 +0000

    Fix absolute path in script

--- scripts/populate.py
+++ scripts/populate.py
@@ -1,16 +1,17 @@
 import csv
 from inventario_app.models import Product
+import pathlib
 
 def run():
-    with open('/Users/rjnocelli/Desktop/dev/django/django_projects/ecommerce/scripts/product_data.csv', 'r') as csv_file:
+    path_of_this_file = str(pathlib.Path(__file__).parent.absolute())
+    with open(f'{path_of_this_file}/product_data.csv', 'r') as csv_file:
         f = csv.reader(csv_file, delimiter=',')
-
         for product in f:
             name = product[0]
             price = int(f"{product[1]}")
             description = product[2]
             p = Product.objects.get_or_create(name=name, price=price, description=description)
             print(f"product {p[0].name} created")
-            
 
-    
+
+
