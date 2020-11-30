3d629111e37f8e9888593c0faca7813eaa7bd4db
Santiago Seifert <aquilescanta@google.com>
2020-11-30 21:27:23 +0000

    Add 127.0.0.1 to allowed hosts

--- inventario/settings.py
+++ inventario/settings.py
@@ -13,9 +13,9 @@ SECRET_KEY = 'no-1lmc8ehgdga-x_ptlw^97#7tv)j1=_pz5-8iq-7c=uy*3(e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.8','localhost']
+ALLOWED_HOSTS = ['192.168.1.8','localhost', '127.0.0.1']
 
-CORS_ORIGIN_ALLOW_ALL = True 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # CAPTCHA
 
