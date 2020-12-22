94c2f3f5f2a115cea4e323b328edebb6d4b672c1
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2020-12-22 08:35:21 -0300

    Amend settings.py

--- inventario/settings.py
+++ inventario/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
+# STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
