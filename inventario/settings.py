25af6725085d2ba5194bf787f78a769aab3c03a6
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2020-12-22 15:56:08 -0300

    Conf

--- inventario/settings.py
+++ inventario/settings.py
@@ -136,9 +136,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/images/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 
