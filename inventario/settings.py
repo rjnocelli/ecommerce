8901e564af0677224f6b188a5d748bf9c0e2cc08
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2020-12-24 19:49:09 -0300

    Conf

--- inventario/settings.py
+++ inventario/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/images/'
 
