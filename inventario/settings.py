2058482865a5ff2199be7f46afa5454c92563052
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-12 15:01:30 -0300

    Change settings.STATIC_ROOT

--- inventario/settings.py
+++ inventario/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
