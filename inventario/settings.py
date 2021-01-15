7c548f971c94f55a5854dd3e5ea5151e9ed0b58e
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-14 21:51:40 -0300

    Add domain to allowed hosts

--- inventario/settings.py
+++ inventario/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'no-1lmc8ehgdga-x_ptlw^97#7tv)j1=_pz5-8iq-7c=uy*3(e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.241.137.29']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.241.137.29', 'www.funesdulceria.com.ar']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
