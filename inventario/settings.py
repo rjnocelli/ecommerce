56c1e6a3f1093d9c7dbfed626f564205b048a534
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-15 17:46:07 -0300

    Amend secret key

--- inventario/settings.py
+++ inventario/settings.py
@@ -9,10 +9,10 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG','debug')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.241.137.29', 'www.funesdulceria.com.ar','funesdulceria.com.ar']
 
