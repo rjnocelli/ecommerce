4ef994cc702ad99b52aaa26ca525564aa14405c1
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-16 14:24:35 -0300

    Delete settings and manage folders

--- inventario/settings.py
+++ inventario/settings.py
@@ -1,8 +1,9 @@
 import os
 from os.path import join
 from dotenv import load_dotenv
-load_dotenv()
+import environ
 
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
@@ -10,7 +11,8 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = False
+env = environ.Env()
+DEBUG = env.bool('DEBUG',default=False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.241.137.29', 'www.funesdulceria.com.ar','funesdulceria.com.ar']
 
