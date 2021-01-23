49212199f33b3f51a58f9992d3db732c8cfaff2a
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-23 20:24:30 -0300

    testing

--- inventario_app/views.py
+++ inventario_app/views.py
@@ -174,11 +174,10 @@ def order_confirmation(request):
                 messages.info(request, 'Orden enviada. Nos contactaremos con usted para ultimar detalles del pedido.')
                 # return render(request, 'inventario_app/index.html', {'order_completed': True})
                 request.session['order_completed'] = True
-                return redirect('inventario_app/index.html')
-                # return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect('/')
         else:
             messages.warning(request, 'No puede enviar una orden vacia.')
             request.session['order_failed'] = True
-            return redirect('inventario_app/index.html')
+            return HttpResponseRedirect('/')
     context = {"form": form}
     return render(request, 'inventario_app/email_confirmation.html', context)
